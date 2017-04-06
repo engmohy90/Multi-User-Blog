c65f09e3bdd4ab2b9aaca13a481fde1aace63900
engmohy90 <engmohyeldean@gmail.com>
2017-04-06 17:40:16 +0200

    py comment##

--- blog.py
+++ blog.py
@@ -16,20 +16,24 @@ secret = "mohy"
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 
 
+# check username match the regular exp
 def valid_username(u):
     return USER_RE.match(u)
 PASSWORD_RE = re.compile("^.{3,20}$")
 
 
+# check password match the regular exp
 def valid_password(p):
     return PASSWORD_RE.match(p)
 EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
 
 
+# check email match the regular exp
 def valid_email(e):
     return EMAIL_RE.match(e)
 
 
+# hashing the cookies
 def hmac_secure(h):
     h = str(h)
     x = (hmac.new(secret, h)).hexdigest()
@@ -37,6 +41,7 @@ def hmac_secure(h):
     return y
 
 
+# check if cookie exist
 def secure_cookie(h):
     z = h.split("|")[0]
     if h == hmac_secure(z):
@@ -45,6 +50,7 @@ def secure_cookie(h):
         return None
 
 
+# hashing the password for safe store at db
 def make_pw_hash(pw):
     salt = ''.join(random.choice(string.letters) for x in xrange(5))
     pw = str(pw)
@@ -52,6 +58,7 @@ def make_pw_hash(pw):
     return "%s|%s" % (h, salt)
 
 
+# check if password as we hased in our db
 def pass_check_salt(pw, h):
     salt = h.split("|")[1]
     hash_p = hashlib.sha256(pw+salt).hexdigest()
@@ -61,40 +68,13 @@ def pass_check_salt(pw, h):
         return False
 
 
-def del_post(key_id, cookies):
-    key_id = int(key_id)
-    x = Post.get_by_id(key_id)
-    user_id = secure_cookie(cookies)
-    if user_id:
-        user_n = Users.get_by_id(int(user_id))
-    else:
-        error = "this post belong to %s you cannot delete" % x.username
-        return error
-    user_name = user_n.username
-
-    if x.username == user_name:
-        x.key.delete()
-        error = "post deleted"
-        return error
-    else:
-        error = "this post belong to %s you cannot delete" % x.username
-        return error
+# orender python code in templet
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
 
-def edit_post(key_id, cookies):
-        key_id = int(key_id)
-        x = Post.get_by_id(key_id)
-        user_id = secure_cookie(cookies)
-        if not user_id:
-            error = "please log in first"
-            return error
-        user_n = Users.get_by_id(int(user_id))
-        user_name = user_n.username
-        if not user_name == x.username:
-            error = " this  post does not belong to you"
-            return error
-
-
+# making db for usernames of our blog
 class Users(ndb.Model):
 
     username = ndb.StringProperty(required=True)
@@ -103,6 +83,7 @@ class Users(ndb.Model):
     created = ndb.DateTimeProperty(auto_now=True)
 
 
+# making db for comment with same post id to link post with comment
 class Comment(ndb.Model):
 
     c_username = ndb.StringProperty(required=True)
@@ -111,6 +92,7 @@ class Comment(ndb.Model):
     comment = ndb.TextProperty(required=True)
 
 
+# make db for post with same username as login to like user with post
 class Post(ndb.Model):
 
     title = ndb.StringProperty(required=True)
@@ -123,6 +105,7 @@ class Post(ndb.Model):
     nunlike = ndb.IntegerProperty()
     nlike = ndb.IntegerProperty()
 
+# search for coment via post id
     def render_post(post):
         post_id = post.key.id()
         comments = ndb.gql("SELECT * FROM Comment WHERE post_id=%d"
@@ -130,11 +113,7 @@ class Post(ndb.Model):
         return render_str("post.html", data=post, comments=comments)
 
 
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
-
-
+# easy render python code in templete and write the html code 
 class Handler(webapp2.RequestHandler):
 
     def write(self, *a, **kw):
@@ -150,18 +129,19 @@ class Handler(webapp2.RequestHandler):
 
         self.write(self.render_str(template, **kw))
 
+    # save likes in db also check if illegibility for makeing like
     def like_post(self, like, login_id):
 
         post_query = Post.get_by_id(int(like))
         login_query = Users.get_by_id(int(login_id))
         u = login_query.username
         if u == post_query.username:
-
-            return self.write("this is your post")
+            error = "this is your post"
+            return error
 
         if u in post_query.like:
-
-            return self.write("you liked this before")
+            error = "you liked this before"
+            return error
         if u in post_query.unlike:
             list_unlike = post_query.unlike
             list_unlike.remove(u)
@@ -180,15 +160,18 @@ class Handler(webapp2.RequestHandler):
             post_query.put()
             return self.redirect("/")
 
+    # save unlikes in db also check if illegibility for makeing unlike
     def unlike_post(self, unlike, login_id):
 
         post_query = Post.get_by_id(int(unlike))
         login_query = Users.get_by_id(int(login_id))
         u = login_query.username
         if u == post_query.username:
-            return self.write("this is your post")
+            error = "this is your post"
+            return error
         if u in post_query.unlike:
-            return self.write("you unliked this before")
+            error = "you unliked this before"
+            return error
         if u in post_query.like:
             list_like = post_query.like
             list_like.remove(u)
@@ -205,11 +188,41 @@ class Handler(webapp2.RequestHandler):
             post_query.put()
             return self.redirect("/")
 
+    # delete post from db
+    def del_post(self, key_id, cookies):
+        key_id = int(key_id)
+        x = Post.get_by_id(key_id)
+        user_id = secure_cookie(cookies)
+        if not user_id:
+            return self.redirect("/login")
+        user_n = Users.get_by_id(int(user_id))
+        user_name = user_n.username
+        if x.username == user_name:
+            x.key.delete()
+            error = "post deleted"
+            return error
+        else:
+            error = "this post belong to %s you cannot delete" % x.username
+            return error
 
+    def edit_post(self, key_id, cookies):
+        key_id = int(key_id)
+        x = Post.get_by_id(key_id)
+        user_id = secure_cookie(cookies)
+        if not user_id:
+            return self.redirect("/login")
+        user_n = Users.get_by_id(int(user_id))
+        user_name = user_n.username
+        if not user_name == x.username:
+            error = " this  post does not belong to you"
+            return error
+
+
+# mainpage which contain 10 latest created post
 class MainPage(Handler):
 
     def get(self):
-        posts = Post.query().order(-Post.last_modified).fetch(10)
+        posts = Post.query().order(-Post.created).fetch(10)
         user = ""
         user_id = self.request.cookies.get("user_id", "1234|6547")
         check_cookie = secure_cookie(user_id)
@@ -221,7 +234,11 @@ class MainPage(Handler):
 
             self.render_html("frontpage.html", user=user, posts=posts)
 
+    # check which button clicked (like or edite or deleted or..) and
+    # make right dession 
     def post(self):
+        comdel = self.request.get("comdel")
+        comedit = self.request.get("comedit")
         comment = self.request.get("comment")
         postcom_id = self.request.get("post_id")
         del_key = self.request.get("del")
@@ -230,24 +247,40 @@ class MainPage(Handler):
         unlike = self.request.get("unlike")
         my_cookie = self.request.cookies.get("user_id", "1234|6547")
         login_id = secure_cookie(my_cookie)
-        if del_key and login_id:
-            del_result = del_post(del_key, my_cookie)
-            self.write(del_result)
-        if edit_key and login_id:
-            edit_result = edit_post(edit_key, my_cookie)
+        posts = Post.query().order(-Post.created).fetch(10)
+        user = ""
+        if login_id:
+            user_name = Users.get_by_id(int(login_id))
+            user = user_name.username
+
+        if del_key:
+            del_result = self.del_post(del_key, my_cookie)
+            self.render_html("frontpage.html", user=user, posts=posts,
+                             error=del_result)
+        if edit_key:
+            edit_result = self.edit_post(edit_key, my_cookie)
             if edit_result:
-                self.write(edit_result)
+                self.render_html("frontpage.html", user=user, posts=posts,
+                                 error=edit_result)
             else:
                 self.redirect("/edit?key=%s" % edit_key)
         if unlike:
+            unlike_click = None
             if login_id:
-                self.unlike_post(unlike, login_id)
+                unlike_click = self.unlike_post(unlike, login_id)
+            if unlike_click is not None:
+                self.render_html("frontpage.html", user=user,
+                                 posts=posts, error=unlike_click)
             else:
                 self.redirect("/login")
 
         if like:
+            like_click = None
             if login_id:
-                self.like_post(like, login_id)
+                like_click = self.like_post(like, login_id)
+            if like_click is not None:
+                self.render_html("frontpage.html", user=user, posts=posts,
+                                 error=like_click)
             else:
                 self.redirect("/login")
 
@@ -261,8 +294,24 @@ class MainPage(Handler):
 
             else:
                 self.redirect("/login")
+        if comdel:
+            coment_q = Comment.get_by_id(int(comdel))
+            if coment_q.c_username == user:
+                coment_q.key.delete()
+                self.render_html("frontpage.html", user=user, posts=posts)
+            else:
+                self.render_html("frontpage.html", user=user, posts=posts,
+                                 error="you cannot delete this comment")
+        if comedit:
+            coment_q = Comment.get_by_id(int(comedit))
+            if coment_q.c_username == user:
+                self.redirect("/comedit?id=%s" % coment_q.key.id())
+            else:
+                self.render_html("frontpage.html", user=user, posts=posts,
+                                 error="you cannot edite this comment")
 
 
+# check for user then add cookies if user is ok
 class LogIn(Handler):
 
     def get(self):
@@ -289,6 +338,7 @@ class LogIn(Handler):
                              error="user name and password are not correct")
 
 
+# store new user in our db
 class SignUp(Handler):
 
     def get(self):
@@ -335,6 +385,7 @@ class SignUp(Handler):
             self.redirect("/welcome")
 
 
+# wellcome for new user
 class Welcome(Handler):
 
     def get(self):
@@ -347,6 +398,7 @@ class Welcome(Handler):
             self.render_html("signup.html")
 
 
+# adding new post to our db by username
 class NewPost(Handler):
 
     def get(self):
@@ -354,7 +406,7 @@ class NewPost(Handler):
         cookie_get = self.request.cookies.get("user_id", "1234|6547")
         check_cookie = secure_cookie(cookie_get)
         if check_cookie is None:
-            self.render_html("newpost.html", login="please login first")
+            self.redirect("/login")
         else:
             self.render_html("newpost.html")
 
@@ -384,6 +436,7 @@ class NewPost(Handler):
             self.render_html("newpost.html", **error)
 
 
+# view post alone t
 class MyNewPost(Handler):
 
     def get(self, i):
@@ -395,6 +448,7 @@ class MyNewPost(Handler):
         self.render_html("mynewpost.html", title=title, content=content)
 
 
+# view only the login user posts
 class MyPosts(Handler):
 
     def get(self):
@@ -424,6 +478,7 @@ class MyPosts(Handler):
                 self.redirect("/edit?key=%s" % edit_key)
 
 
+# clear cookies and logout
 class LogOut(Handler):
 
     def get(self):
@@ -431,6 +486,7 @@ class LogOut(Handler):
         self.redirect("/signup")
 
 
+# edit post 
 class Edit(Handler):
 
     def get(self):
@@ -474,6 +530,35 @@ class Edit(Handler):
         else:
             self.render_html("edit.html", **error)
 
+
+# edit comment of the posts
+class Comedit(Handler):
+
+    def get(self):
+        comment_id = self.request.get("id")
+        coment_q = Comment.get_by_id(int(comment_id))
+        self.render_html("editcomment.html", comedit=coment_q)
+
+    def post(self):
+        comment = self.request.get("comment")
+        comedit = self.request.get("comedit")
+        cancle = self.request.get("cancle")
+        my_cookie = self.request.cookies.get("user_id", "1234|6547")
+        login_id = secure_cookie(my_cookie)
+        user = ""
+        if login_id:
+            user_name = Users.get_by_id(int(login_id))
+            user = user_name.username
+        if comedit:
+            coment_q = Comment.get_by_id(int(comedit))
+            if user == coment_q.c_username:
+                coment_q.comment = comment
+                coment_q.put()
+                self.redirect("/")
+        else:
+            self.redirect("/")
+
+
 app = webapp2.WSGIApplication([("/", MainPage),
                                ("/login", LogIn),
                                ("/signup", SignUp),
@@ -483,4 +568,5 @@ app = webapp2.WSGIApplication([("/", MainPage),
                                ("/myposts", MyPosts),
                                ("/mynewpost/([0-9]+)", MyNewPost),
                                ("/edit", Edit),
+                               ("/comedit", Comedit)
                                ], debug=True)
